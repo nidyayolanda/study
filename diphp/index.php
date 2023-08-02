20c5fcd864348dc3e38d5b365818ea06212b6d5d
nidyayolanda <nidyayolanda36@gmail.com>
2023-08-02 15:29:41 +0700

    kendali

--- diphp/index.php
+++ diphp/index.php
@@ -1,5 +1,55 @@
+<!--ini percabangan-->
+<?php
+$t = date ("H");
+if ($t< "20"){
+    echo " have a good day! <br>";
+}
+else{
+    echo " have a good night!";
+}
+?>
+================ ini percabangan ==================</br> 
+
+<!-- ini perulangan-->
+<?php
+$matauang = array ("Rupiah", "Dollar","Ringgit","Bath","Real");
+foreach ($matauang as $value)
+{
+    echo "$value <br>";
+}
+?>
+================ ini perulangan ==================</br>  
+
+<!-- ini perpindahan -->
+<?php
+for ($x = 1; $x < 10 ; $x++){
+    if ($x == 6){
+    break;
+}
+echo "ini ayamku ke: $x <hr>";
+}
+?>
+================ ini perpindahan ==================</hr>  
+
+<!-- ini function -->
+<?php
+function Heroname ($hname)
+{
+    echo "$hname Goldlane. <br>";
+}
+Heroname ("<br> Miya");
+Heroname ("Gusion");
+Heroname ("Beatrix");
+Heroname ("Kimmy");
+Heroname ("Bruno");
+?>
+
+================ ini function ==================</br>  
+<!-- ini array -->
 
 <?php
 $fruit = array("Apel", "Anggur", "Tomat","Terong","Talas","Pisang","Nanas","Chery","Leci","Rambutan","Melon","Kiwi");
-echo count($fruit);
+echo count($fruit) ;
 ?>
+<br>
+================ ini array ==================</br> 
